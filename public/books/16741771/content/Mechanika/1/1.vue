c883e644e99d42b4bd4f197161629142fa8161c6
Mateusz Słotwiński <78279510+mateusz-slotwinski@users.noreply.github.com>
2022-10-06 23:38:34 +0200

    natężenie?

--- public/books/16741771/content/Mechanika/1/1.vue
+++ public/books/16741771/content/Mechanika/1/1.vue
@@ -77,6 +77,67 @@
       świata. Fizyka znowu stała się potrzebna, a świat znowu miał dla nas
       tajemnice do odkrycia. Znowu staliśmy się odkrywcami świata.
     </Section>
+    <h3>Natężenie? No prawie...</h3>
+    <Section>
+      Użycie słowa natężenie promieniowania miało na celu uprościć wprowadzenie
+      - tak naprawdę chodzi o radiancje spektralną. Jeśli idziesz tylko
+      tematami, które omijają zaawansowane zagadnienia, nie załamuj się. Na
+      spokojnie, zaraz pokażę ci czym ona jest. Dla tych, którzy już to
+      przerobili będzie to przypomnienie. Przejdziemy od początku do końca
+      prawie całą radiometrię.
+    </Section>
+    <Section>
+      Światło i ogólnie promieniowanie elektromagnetyczne z pewnością przenosi
+      jakąś energię - oznaczmy ją
+      <Math>Q_e</Math> (e od słowa energia tak dla ułatwienia). Skoro mamy
+      energię to policzmy w takim razie moc promieniowania. Ta moc nazywa się
+      strumieniem promieniowania i oznaczamy grecką literą <Math>\Phi_e</Math>,
+      zatem:
+      <Math center="true" nr="5.1">
+        {\displaystyle \Phi _{e} = \frac{d}{dt}Q_e }
+      </Math>
+    </Section>
+    <Section>
+      Teraz przyszedł czas na natężenie promieniowania. Definiujemy go jako
+      stosunek natężenia światła do kąta<Math>\Omega</Math>, który oświetla.
+      <Math center="true" nr="5.2">
+        {\displaystyle I_{e} = \frac{d}{d\Omega} \Phi _{e} }
+      </Math>
+      Dlaczego kąt jest istotny? Kiedy jeździsz samochodem masz dwa główne
+      rodzaje świateł: mijania (zielona ikonka, tzw. krótkie) i drogowe
+      (niebieska ikonka, tzw. długie). Moc ich promieniowania jest taka sama,
+      tym czym się różnią to kątem, jaki oświetlają. Światła krótkie oświetlają
+      znacznie większą część drogi, ale natężenie promieniowania jest mniejsze.
+      Światła długie zmniejszają kąt działania, dlatego ich natężenie jest
+      większe.
+    </Section>
+    <Section>
+      Zbliżamy się już do końca. Czym jest Radiancja? Skoro już mamy światło o
+      danym natężeniu to teraz sprawdźmy na jaką powierchnie pada. Bardziej nas
+      ono oślepi patrząc się na nie z kilku centymetrów niż z kilku metrów,
+      prawda? No to podzielmy natężenie światła przez powierzchnię, jaką
+      oświetla i uwzględnijmy jeszcze kąt <Math>\theta</Math> pod jakim na nie
+      patrzymy. Patrząc na wprost światło nas oświetli najbardziej, natomiast
+      patrząc pod kątem 90 stopni w ogóle nie świeci nam w oczy. Idealnie więc
+      nada się tu funkcja cosinus.
+      <Section>
+        <Math center="true" nr="5.4">
+          {\displaystyle L_{e} = \frac{dI_e}{dScos\theta} }
+        </Math>
+      </Section>
+    </Section>
+    <Section>
+      No i przyszedł na finał - radiancja spektralna. Czym ona jest? Każda fala
+      elektromagnetyczna ma jakąś długość, prawda? No to po prostu dzielimy
+      radiancję przez długość tej fali. Oto przepis na radiancję spektralną. To
+      jest stosunek radiancji do długości fal, więc dodajemy do radiancji po
+      prostu indeks
+      <Math>\lambda</Math>
+      <Math center="true" nr="5.4">
+        {\displaystyle L_{\lambda} = \frac{dL_e}{d\lambda} }
+      </Math>
+    </Section>
+
     <h3>Promieniowanie ciała doskonale czarnego</h3>
     <Section>
       Już w XIXw. zostało udowodnione, że natężenie promieniowania termicznego
