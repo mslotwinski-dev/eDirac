7cf075f409c1334f8298c2b31b8b9e879b40e50f
Mateusz Słotwiński <78279510+mateusz-slotwinski@users.noreply.github.com>
2022-10-09 17:07:27 +0200

    fluid

--- public/books/16741767/content/Hydrodynamika/Wlasciwosci/1.vue
+++ public/books/16741767/content/Hydrodynamika/Wlasciwosci/1.vue
@@ -1,5 +1,61 @@
 <template>
   <Article>
     <Begin nr="1.1" advanced="0"> Czym jest płyn? </Begin>
+    <h3>Z czym to się je?</h3>
+    <Section>
+      Hydrodynamika (mechanika płynów) to dział mechaniki ośrodków ciągłych,
+      który obejmuje zagadnienia równowagi i ruchu płynów oraz wpływu płynów na
+      ściany i osadzone w nich ciała. Jako dyscyplina teoretyczna mechanika
+      płynów podlega tym samym prawom, co mechanika ciał stałych (omówiona w
+      następnej części jako teoria sprężystości). Jednak problemy związane z
+      ruchem płynów i równowagą są bardziej złożone niż problemy mechaniki ciała
+      stałego. Wynika to głównie z właściwości płynów rzeczywistych. W tym
+      przypadku metody teoretyczne muszą być uzupełnione metodami
+      eksperymentalnymi. Dlatego doświadczenie odgrywa ważną rolę w mechanice
+      płynów, ponieważ analiza matematyczna opisująca przepływ płynów
+      rzeczywistych pozwala zrozumieć ich naturę tylko w sposób bardzo ogólny.
+    </Section>
+    <Section>
+      Pole zastosowań teoretycznej i eksperymentalnej mechaniki płynów w wielu
+      specjalistycznych dziedzinach techniki jest bardzo rozległe. Z przepływami
+      takich czynników, jak gaz, woda, olej, para, spotykamy się w energetyce,
+      maszynach elektrycznych, inżynierii chemicznej i środowisku (wodociągi,
+      kanalizacja, ogrzewanie, wentylacja, klimatyzacja, odpylanie itp.),
+      lotnictwo, przemysł stoczniowy, przemysłowe urządzenia przepływowe,
+      urządzenia chłodnicze, transport rurowy cieczy i gazów, układy
+      hydrauliczne. Mechanika płynów tworzy podstawy teoretyczne i doświadczalne
+      wymienionych dziedzin
+    </Section>
+    <h3>Czym jest płyn?</h3>
+    <Section>
+      Termin płyn obejmuje <b>zarówno ciecze, jak i gazy</b>. Są to ciała,
+      których wspólną cechą jest to, że nie mogą zachować swojego kształtu (przy
+      bardzo małej sprężystości), i stąd bardzo łatwo jest zmienić wzajemne
+      położenie poszczególnych elementów płynu w zbiornuku. Ta właściwość
+      odróżnia ciecze od ciał stałych, które mogą zmieniać kształt tylko pod
+      wpływem dużych sił zewnętrznych.
+    </Section>
+    <Section>
+      Ciecze różnią się od gazów tym, że nie mają tendencji do ściskania i
+      rozszerzania się w nieskończoność (sprężanie i rozprężanie). W związku z
+      tym mają wewnętrzną objętość, która zmienia się nieznacznie pod siłami
+      zewnętrznymi. Ciecze są bardzo słabo ściśliwe, natomiast gazy
+      charakteryzują się dużą ściśliwością i w normalnych warunkach zajmują całą
+      przestrzeń, w której się znajdują, chociaż czasami gazy można również
+      uznać za płyny nieściśliwe. W gruncie rzeczy, równania mechaniki płynów są
+      te same dla cieczy i gazów, jednak czasem będziemy je uszczególniać albo
+      tylko dla cieczy albo tylko dla gazów, bo wtedy przyjmują prostszą postać.
+    </Section>
+    <h3>Ośrodek ciągły</h3>
+    <Section>
+      W mechanice płynów, podobnie jak w mechanice ciała stałego, płyn
+      rzeczywisty zastępuje się modelem teoretycznym. Jeśli nie uwzględniamy
+      struktury cząsteczkowej i nieuporządkowanych ruchów cząsteczek, przyjmuje
+      się, że nasz model płynu jest ośrodkiem ciągłym. Płyn ten jest materią
+      ciągłą, wypełniającą przestrzeń w sposób ciągły (tzn. dowolnie małe
+      otoczenie punktu w tej przestrzeni zachowuje jej właściwości). Przy
+      założeniu, że płyn to ośrodek ciągły ustalono podstawowe równania
+      hydrodynamiki.
+    </Section>
   </Article>
 </template>
--- public/books/16741767/content/Hydrodynamika/Wlasciwosci/2.vue
+++ public/books/16741767/content/Hydrodynamika/Wlasciwosci/2.vue
@@ -1,5 +1,23 @@
 <template>
   <Article>
     <Begin nr="1.2" advanced="0"> Właściwości płynów </Begin>
+    <h3>GĘSTOŚĆ</h3>
+    <Section>
+      Przypomnijmy czym jest gęstość. Od teraz praktycznie przez całą
+      hydrodynamikę będzie to najważniejsza właściwość badanych ciał tak jak w
+      mechanice jest to masa. Gęstość w punkcie definiujemy jako:
+    </Section>
+    <Math center="true" nr="1.1"> {\displaystyle \rho = \frac{dm}{dV}} </Math>
+    <Section>
+      Jeśli płyn jest poddawany stałemu ciśnieniu, jego gęstość w każdym punkcie
+      jest jednakowa i po prostu równa m/V. Rzadziej używane jest pojęcie
+      ciężaru właściwego, który jest ilorazem gęstości i przyśpieszenia
+      ziemskiego:
+    </Section>
+    <Math center="true" nr="1.1"> {\displaystyle \gamma = \rho g} </Math>
+    <Section>
+      W ogólnym przypadku gęstość płynu zależy od czasu, temperatury i
+      ciśnienia.
+    </Section>
   </Article>
 </template>
