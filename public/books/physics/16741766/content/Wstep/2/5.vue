f53558bc55001c592f59d2cf8fd708a7203a1937
Mateusz Słotwiński <78279510+mateusz-slotwinski@users.noreply.github.com>
2022-10-30 16:33:14 +0100

    piwo to moje paliwo

--- public/books/physics/16741766/content/Wstep/2/5.vue
+++ public/books/physics/16741766/content/Wstep/2/5.vue
@@ -13,6 +13,51 @@
       cząstki w jednym miejscu. Czymś takim jest model standardowy.
     </Section>
     <Image src="wstep/2.png">Model standardowy</Image>
+    <h3>Fermiony</h3>
+    <Section>
+      Po lewej stronie kolorem niebieskim i fioletowym oznaczono cząstki zwane
+      fermionami. Są one czymś budulcem w pełym tego słowa znaczeniu. Dzielimy
+      je na kwarki (fioletowy) i leptony (niebieski). Zacznijmy od kwarków. Jako
+      pierwsze zostały odkryte kwarki górny (u) oraz dolny (d). Są one
+      fundamentalnym budulcem jąder atomowych, a więc i całego znanego
+      wszechświata.
+    </Section>
+    <Section>
+      Hipotezę istnienia kwarków jako elementarnych składników materii wysunęli
+      niezależnie od siebie Murray Gell-Mann i George Zweig w 1964 roku. Nazwę
+      zaproponował Gell-Mann. Słowo quark pochodzi ze zdania
+      <i>Three quarts for Muster Mark!</i> z powieści „Finnegans Wake” autorstwa
+      Jamesa Joyce’a, a w owym czasie Gell-Mann i Zweig postulowali istnienie
+      właśnie trzech cząstek – u, d i s – oraz ich antycząstek. Cytat według
+      niektórych interpretacji mówi o trzech kwartach piwa, co w przeliczeniu na
+      nasze jednostki daje około 2,75l. Mark to twardy zawodnik. Chcąc odkryć
+      coś wielkiego musisz się na nim wzorować.
+    </Section>
+    <Section>
+      Szansa na potwierdzenie istnienia kwarków pojawiła się w 1968 podczas
+      eksperymentów z nieelastycznym rozpraszaniem elektronów. Przy mniejszych
+      energiach elektrony odbijały się od protonu tak, jakby był on jednorodną
+      elastyczną kulką, jednak przy wzroście energii zderzeń, gdy pęd elektronów
+      zwiększano na tyle, że długość fali materii tych elektronów stała się
+      mniejsza od rozmiarów protonu, elektrony zaczęły rozpraszać się w taki
+      sposób, jakby zderzały się z punktowymi obiektami wewnątrz protonu. Jest
+      to efekt analogiczny do obserwowanego 50 lat wcześniej w doświadczeniu
+      Rutherforda (niezgodność kąta odbicia z oczekiwaniami). Doświadczenia te
+      wykazały, że protony (podobnie jak neutrony, o czym przekonano się
+      później) mają wewnętrzną strukturę
+    </Section>
+    <Section>
+      Czym są jednak ten trzeci kwark s? Jest to tak zwany kwark dziwny, a
+      większe cząstki, które buduje są nazywane cząstkami dziwnymi. Rozwój
+      fizyki cząstek elementarnych pozwolił na odkrycie jeszcze trzech kwarków -
+      powabnego (c), niskiego (b, zwanym również pięknym) oraz szczytowego (t,
+      zwanym również prawdziwym). Masa każdego kolejnego była coraz wyższa. O
+      ile kwark u ma około 2-3 MeV/c<M s="^2" />, u dziwnego było to już 100, u
+      powabnego tysiąc to u niskiego sięga 4 tysięcy MeV/c<M s="^2" />. Całkiem
+      dużo, nie? To teraz sobie wyobraź, że masa tego ostatniego, szczytowego,
+      to ponad 170 tysięcy MeV/c<M s="^2" />. Powiedzieć, że to bydle to nic nie
+      powiedzieć.
+    </Section>
     <h3>Atomy w ruchu</h3>
     <Section>
       Aby zilustrować siłę idei atomowej, załóżmy, że z boku mamy kroplę wody o
