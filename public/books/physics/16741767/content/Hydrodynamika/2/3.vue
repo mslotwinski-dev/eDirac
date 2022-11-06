fba6c8abbbc7ac7d2ca6eaca5b7ccfd6439627f2
Mateusz Słotwiński <78279510+mateusz-slotwinski@users.noreply.github.com>
2022-11-06 17:53:51 +0100

    u-rurka

--- public/books/physics/16741767/content/Hydrodynamika/2/3.vue
+++ public/books/physics/16741767/content/Hydrodynamika/2/3.vue
@@ -12,7 +12,19 @@
       Rozpatrzymy kilka przypadków równowagi cieczy w naczyniach połączonych.
       Najprostszym przypadkiem jest naczynie połączone otwarte, w którym
       znajduje się ciecz jednorodna. Poziom cieczy w naczyniach znajduje się
-      wtedy na tej samej wysokości.
+      wtedy na tej samej wysokości .
     </Section>
+    <Image src="hydrodynamika/3.png" />
+    <Section>
+      W przypadku gdy mamy dwie różne ciecze w jednym naczyniu powinniśmy
+      skorzystać ze wzoru na ciśnienie hydrostatyczne.
+    </Section>
+    <Math>p_0 + \varrho_1 g h_1 = p_0 + \varrho_2 g h_2</Math>
+    <Section>
+      W przypadku otwartych naczyń skróci nam się ciśnienie początkowe. Łatwo
+      zauważyć, że poleci nam również g, gdyż jest po obu stronach. Ostateczne
+      równanie wygląda zatem tak:
+    </Section>
+    <Formula nr="2.8">\varrho_1 h_1 = \varrho_2 h_2</Formula>
   </Article>
 </template>
