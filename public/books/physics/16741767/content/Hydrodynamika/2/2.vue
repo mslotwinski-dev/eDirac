0d092dbe870d8c8423dc2356c9a76fc4d38fb750
Mateusz Słotwiński <78279510+mateusz-slotwinski@users.noreply.github.com>
2022-11-06 16:29:36 +0100

    u-rurka

--- public/books/physics/16741767/content/Hydrodynamika/2/2.vue
+++ public/books/physics/16741767/content/Hydrodynamika/2/2.vue
@@ -47,5 +47,42 @@
       naczynia to suma ciśnienie atmosferycznego i hydrostatycznego, stąd
       dodajemy do wyniku <M c="p_0" />
     </Section>
+    <h3>Gwiazda *</h3>
+    <Section>
+      Wyprowadźmy na koniec równanie równowagi bardzo dużej masy, której części
+      utrzymują się razem siłami grawitacyjnymi. Niech ϕ oznacza potencjał
+      grawitacyjny pola wytwarzanego przez ciecz. Spełnia on następujące
+      równanie różniczkowe:
+    </Section>
+    <Formula nr="2.5">
+      \displaystyle \bigtriangleup \phi = 4\pi G \varrho
+    </Formula>
+    <Section>
+      Natężenie pola grawitacyjnego jest wtedy równe -<M
+        c="\bigtriangledown \phi"
+      />, więc siła działająca na ciecz jest wtedy równa
+      <M c="- \varrho \bigtriangledown \phi" />. Stąd warunkiem równowagi jest:
+    </Section>
+    <Math>
+      \displaystyle - \varrho \bigtriangledown \phi = \bigtriangledown p
+    </Math>
+    <Section>
+      Dzieląc równanie stronami przez gęstość, działając na obie jego strony
+      operatorem dywergencji oraz korzystając z równania (2.5) otrzymujemy
+      równanie równowagi gwiazdy w postaci:
+    </Section>
+    <Formula nr="2.6">
+      \displaystyle \bigtriangledown \cdot \bigg( \frac{1}{\varrho}
+      \bigtriangledown p \bigg) = 4\pi G \varrho
+    </Formula>
+    <Section>
+      Jeśli ciało nie obraca się, w równowadze będzie miało kształt kuli, a
+      rozkład gęstości i ciśnienia będzie centralnie symetryczny. Równanie to we
+      współrzędnych sferycznych przybierze wówczas postać:
+    </Section>
+    <Formula nr="2.7">
+      \displaystyle \frac{1}{r^2} \frac{d}{dr} \Bigg( \frac{r^2}{\varrho}
+      \frac{dp}{dr} \Bigg) = 4\pi G \varrho
+    </Formula>
   </Article>
 </template>
--- public/books/physics/16741767/content/Hydrodynamika/2/3.vue
+++ public/books/physics/16741767/content/Hydrodynamika/2/3.vue
@@ -1,42 +1,18 @@
 <template>
   <Article>
-    <Begin nr="2.2" advanced="2"> Równowaga hydrostatyczna w gwiazdach</Begin>
-    <h3>Gwiazda</h3>
+    <Begin nr="2.3" advanced="0"> Naczynia połączone</Begin>
+    <h3>U-rurka</h3>
     <Section>
-      Wyprowadźmy na koniec równanie równowagi bardzo dużej masy, której części
-      utrzymują się razem siłami grawitacyjnymi. Niech ϕ oznacza potencjał
-      grawitacyjny pola wytwarzanego przez ciecz. Spełnia on następujące
-      równanie różniczkowe:
+      Naczyniami połączonymi nazywamy dwa lub więcej naczyń połączonych ze sobą
+      przewodem lub przewodami. Przekroje naczyń powinny być tak duże, by siły
+      spójności i przylegania, działające między ich ścianami przewodów a
+      cząstkami cieczy, nie odgrywały wyczuwalnej roli.
     </Section>
-    <Formula nr="2.5">
-      \displaystyle \bigtriangleup \phi = 4\pi G \varrho
-    </Formula>
     <Section>
-      Natężenie pola grawitacyjnego jest wtedy równe -<M
-        c="\bigtriangledown \phi"
-      />, więc siła działająca na ciecz jest wtedy równa
-      <M c="- \varrho \bigtriangledown \phi" />. Stąd warunkiem równowagi jest:
+      Rozpatrzymy kilka przypadków równowagi cieczy w naczyniach połączonych.
+      Najprostszym przypadkiem jest naczynie połączone otwarte, w którym
+      znajduje się ciecz jednorodna. Poziom cieczy w naczyniach znajduje się
+      wtedy na tej samej wysokości.
     </Section>
-    <Math>
-      \displaystyle - \varrho \bigtriangledown \phi = \bigtriangledown p
-    </Math>
-    <Section>
-      Dzieląc równanie stronami przez gęstość, działając na obie jego strony
-      operatorem dywergencji oraz korzystając z równania (2.5) otrzymujemy
-      równanie równowagi gwiazdy w postaci:
-    </Section>
-    <Formula nr="2.6">
-      \displaystyle \bigtriangledown \cdot \bigg( \frac{1}{\varrho}
-      \bigtriangledown p \bigg) = 4\pi G \varrho
-    </Formula>
-    <Section>
-      Jeśli ciało nie obraca się, w równowadze będzie miało kształt kuli, a
-      rozkład gęstości i ciśnienia będzie centralnie symetryczny. Równanie to we
-      współrzędnych sferycznych przybierze wówczas postać:
-    </Section>
-    <Formula nr="2.7">
-      \displaystyle \frac{1}{r^2} \frac{d}{dr} \Bigg( \frac{r^2}{\varrho}
-      \frac{dp}{dr} \Bigg) = 4\pi G \varrho
-    </Formula>
   </Article>
 </template>
