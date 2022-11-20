5ef945fdf22e4c764cd6b53aea7347d0a2e07080
Mateusz Słotwiński <mateuszslotwinski16@gmail.com>
2022-11-20 23:35:53 +0100

    równia

--- public/books/physics/16741766/content/Mechanika/2/8.vue
+++ public/books/physics/16741766/content/Mechanika/2/8.vue
@@ -17,8 +17,44 @@
     <h3>Równia pochyła</h3>
     <Section>
       Sztandarowym przykładem użycia równań Newtona do rozwiązywania problemów
-      mechanicznych jest równia pochyła.
+      mechanicznych jest równia pochyła. Jest to w zasadzie jakakolwiek płaska
+      powierzchnia pochylona pod pewnym kątem. Zacznijmy od analizy sił.
     </Section>
     <Image src="dynamika/6.png">Rozkład sił na równi pochyłej</Image>
+    <Section>
+      Ciężar ciała na równi jest równy mg, jednak jego nacisk na równie, jak i
+      reakcja równi, są prostopadłe do jej powierzchni i wynoszą
+      <M s="mg \ cos \theta" />. Siła styczna, która zsuwa naszą masę jest równa
+      <M s="mg \ sin \theta" />.
+    </Section>
+    <Section>
+      Zakładając, że siły tarcia nie występują możemy przyrównać siłę zsuwania
+      do wzoru Newtona. W tym przypadku dostajemy:
+    </Section>
+    <Math>\displaystyle ma = mg \ sin \theta</Math>
+    <Section>
+      Co po skróceniu daje nam przyspieszenie, jakie doznaje zsuwane ciało.
+    </Section>
+    <Math>\displaystyle a = g \ sin \theta</Math>
+    <Section>
+      Nie jest to jednak przypadek, jaki nas interesuje. Dołożymy tutaj siłę
+      tarcia. W tym przypadku wypadkowa siła zsuwająca będzie równa
+    </Section>
+    <Math>
+      \displaystyle F = mg \ sin \theta - N \mu = mg \ sin \theta - mg \ cos
+      \theta \mu
+    </Math>
+    <Section>Przyspieszenie zsuwającego ciężarku jest wtedy równe</Section>
+    <Formula nr="4.11">
+      \displaystyle a = g (sin \theta - \mu cos \theta)
+    </Formula>
+    <Section>
+      Jako ćwiczenie dla bardziej zaawanaowanych możesz spróbować udowodnić ten
+      wzór, który również możesz stosować.
+    </Section>
+    <Math>
+      \displaystyle a = g \sqrt{ 1 + \mu^2} \ sin(\theta + arctg (-\mu))
+    </Math>
+    <Section>Dla kątów bliskich 0 możesz postąpić w ten sposób: </Section>
   </Article>
 </template>
