c96295d15cd8cef0b5fa9259a5af2c275d8b47e6
Mateusz Słotwiński <mateuszslotwinski16@gmail.com>
2022-12-08 21:05:44 +0100

    minecraft

--- public/books/physics/16741766/content/Mechanika/5/1.vue
+++ public/books/physics/16741766/content/Mechanika/5/1.vue
@@ -2,5 +2,7 @@
   <Article>
     <Chapter nr="7" book="physics"> Energia </Chapter>
     <Begin nr="7.1" advanced="0"> Energia kinetyczna i praca </Begin>
+    <h3>Energia ciała</h3>
+    <Section>Dziś nie będzie nowego tematu, dziś gram w Minecrafta</Section>
   </Article>
 </template>
