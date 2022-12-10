d2c2380764f44e33428b6780cf07c28d1a229567
Mateusz Słotwiński <mateuszslotwinski16@gmail.com>
2022-12-10 21:14:29 +0100

    energy

--- public/books/physics/16741766/content/Mechanika/5/1.vue
+++ public/books/physics/16741766/content/Mechanika/5/1.vue
@@ -2,7 +2,44 @@
   <Article>
     <Chapter nr="7" book="physics"> Energia </Chapter>
     <Begin nr="7.1" advanced="0"> Energia kinetyczna i praca </Begin>
-    <h3>Energia ciała</h3>
-    <Section>Dziś nie będzie nowego tematu, dziś gram w Minecrafta</Section>
+    <h3>Czym jest energia?</h3>
+    <Section>
+      Zasady dynamiki Newona umożliwiają analizowanie właściwie wszystkich
+      rodzajów ruchów. Może to być jednak bardzo skomplikowane i wymagać
+      szczegółowych paramatrów, których nie znamy. Na przykład wyobraź sobie
+      krążek, który ześlizguje się po pochyłym torze, na którym jest wiele
+      wzniesień i spadków. Przykładowym zadaniem może być obliczenie prędkości
+      na samym końcu ruchu znając jedynie wysokość z jakiej krążek startuje. Czy
+      możesz obliczyć to korzystając jedynie z zasad Newtona? W zasadzie tak,
+      ale musisz dokładnie wiedzieć, jak zmienia się nachylenie toru wzdłuż
+      całej drogi krążka, a obliczenia byłyby skrajnie złożone.
+    </Section>
+    <Section>
+      Już dawno temu naukowcy i inżynierowie doszli do wniosku, że ruch można
+      badać również w inny sposób. Metoda ta stała się tak użyteczna, że
+      znalazła swoje zastosowanie we wszystkich działach fizyki, a nawet w
+      chemii, geografii i biologii. W metodzie tej korzystamy z energii.
+    </Section>
+    <Section>
+      Energia może mieć wiele postaci. Termin energia ma w rzeczywistości tak
+      szerokie znaczenie, że ciężko wskazać jego klarowną definicję. Technicznie
+      rzecz biorąc, jest to wielkość skalarna, charakteryzująca stan, w jakim
+      znajduje się ciało lub układ ciał. Ta definicja jest jednak tak ogólna, że
+      na wiele nam się nie przyda.
+    </Section>
+    <Section>
+      Energia to liczba, jaką przypisujemy do układu ciał, jednak co ona
+      dokładnie reprezentuje? Po pierwsze, jeśli siła zmienia stan jednego z
+      ciał, na przykład wprawiając je w ruch, to liczba ta się zmienia.
+      Zacznijmy od pierwszej i najprostszej postaci energii, jaką znamy, która
+      jest energia kinetyczna.
+    </Section>
+    <Section>
+      Energia kinetyczna (oznaczana <M s="E_k" />, EK lub T) jest zależna od
+      prędkości ciała i od masy. Ciało, które porusza się z określoną prędkością
+      po zderzeniu zmniejsza swoją energię działając przy tym siłą, na ciało, z
+      którym się zderza. Definicja energii jest więc spełniona.
+    </Section>
+    <Formula nr="7.1">E_k = \frac{1}{2}mv^2</Formula>
   </Article>
 </template>
