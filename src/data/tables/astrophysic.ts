9efc5aca9a6d23bd610027515560da98ef3e5cb0
Mateusz Słotwiński <mateuszslotwinski16@gmail.com>
2023-02-14 11:46:09 +0100

    Update astrophysic.ts

--- src/data/tables/astrophysic.ts
+++ src/data/tables/astrophysic.ts
@@ -1,1 +1,13 @@
-export default {}
+export default {
+  '': {
+     Gravity: {
+      content: ['G = 6,674 \\ 30(15) · 10^{-11} \\ \\frac{m^3}{kg \\ s^2}'],
+      in: [
+        'F = G \\frac{Mm}{r^2}',
+        '{\\bigtriangleup \\Phi} = 4\\pi G \\varrho',
+        'G_{\\mu \\nu }+\\Lambda g_{\\mu \\nu }={\\frac {8\\pi }{c^{4}}}GT_{\\mu \\nu }',
+      ],
+      type: 'formula',
+    },
+  }
+}
