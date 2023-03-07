4cb7c755050bb9cf28b4b7f4441bccc4e456d363
Mateusz Słotwiński <mateuszslotwinski16@gmail.com>
2023-03-07 15:03:04 +0100

    Update 3.vue

--- public/books/physics/16741766/content/Wstep/2/3.vue
+++ public/books/physics/16741766/content/Wstep/2/3.vue
@@ -1,6 +1,7 @@
 <template>
   <Article>
     <Begin nr="2.3" advanced="0"> Elektryczność i magnetyzm </Begin>
-    <Section>*Pomiń*</Section>
+    <h3>Odkrycie elektryczności</h3>
+    <Section></Section>
   </Article>
 </template>
