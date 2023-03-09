a0f3ed314c3b43361b9ad43a94f152995dbeca15
Mateusz Słotwiński <mateuszslotwinski16@gmail.com>
2023-03-09 20:05:39 +0100

    scss

--- src/components/Info/Sections.vue
+++ src/components/Info/Sections.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="info-class-global">
+  <div class="global-info-class">
     <Beginnings />
     <Activity />
     <GoalsAndPlans />
@@ -42,56 +42,3 @@ export default defineComponent({
   },
 })
 </script>
-
-<style lang="scss">
-@import '@/styles/index.scss';
-.info-class-global {
-  > div {
-    min-height: 20vh;
-    padding: 10px;
-  }
-  h2 {
-    text-transform: uppercase;
-    text-align: center;
-    font-size: 35px;
-    margin: 10px;
-
-    font-weight: 500;
-    @media (max-width: 1175px) {
-      font-size: 26px;
-      margin: 8px;
-    }
-  }
-
-  h3 {
-    text-align: center;
-    font-size: 22px;
-    margin: 15px;
-    font-weight: 400;
-    @media (max-width: 1175px) {
-      font-size: 16px;
-      margin: auto;
-      width: 80vw;
-    }
-  }
-
-  section {
-    width: 1000px;
-    max-width: 95vw;
-    margin: auto;
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-
-    div {
-      width: 100%;
-      font-size: 16.5px;
-
-      @media (max-width: 1000px) {
-        font-size: 14px;
-      }
-    }
-  }
-}
-</style>
